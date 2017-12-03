3937d423487ed40c499cbe352840a72e28aced22
Sim00n <portalowicz@gmail.com>
2017-12-03 16:36:17 -0500

    Fixed caching.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html> 
+<html lang="en"> 
+	<head> 
+		<meta charset="UTF-8" />
+		<title>APPROACHING OVERLOAD</title>
+ 		
+ 		<link rel="stylesheet" href="src/style.css">
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+		<script type="text/javascript" src="lib/phaser.js?a=<?php echo md5(time()); ?>"></script>
+		<script type="text/javascript" src="lib/custom/phaser-input.js?a=<?php echo md5(time()); ?>"></script>
+		<script type="text/javascript" src="src/config.js?a=<?php echo md5(time()); ?>"></script>
+		<script type="text/javascript" src="src/utils.js?a=<?php echo md5(time()); ?>"></script>
+		<script type="text/javascript" src="src/classes.js?a=<?php echo md5(time()); ?>"></script>
+		<script type="text/javascript" src="src/index.js?a=<?php echo md5(time()); ?>"></script>
+		<script type="text/javascript" src="src/parser.js?a=<?php echo md5(time()); ?>"></script>
+	</head>
+	<body> </body>
+</html>
